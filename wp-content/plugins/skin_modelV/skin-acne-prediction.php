85be13f1308ddcff853f79da35270247dc23542c
Panupong PSN <panupong_s_n@hotmail.com>
2024-09-04 18:42:46 +0700

    Add new files to skin_modelV plugin

--- wp-content/plugins/skin_modelV/skin-acne-prediction.php
+++ wp-content/plugins/skin_modelV/skin-acne-prediction.php
@@ -9,15 +9,21 @@ Author: Your Name
 // Shortcode to display the form
 function sap_prediction_form() {
     return '
-        <form id="sapForm" enctype="multipart/form-data">
-            <label for="sapImage">Please upload an image of the acne-affected area:</label>
-            <input type="file" id="sapImage" name="sapImage" accept="image/*" required>
-            <button type="submit">Predict</button>
-        </form>
-        <img id="uploadedImage" style="display:none; width: 100%; max-width: 400px;"/>
-        <p id="analyzingMessage"></p>
-        <p id="result"></p>
+        <div style="text-align: center; max-width: 600px; margin: 0 auto;">
+            <h2>Analyze</h2>
+            <form id="sapForm" enctype="multipart/form-data">
+                <label for="sapImage">Please upload an image of the acne-affected area:</label>
+                <br>
+                <input type="file" id="sapImage" name="sapImage" accept="image/*" required>
+                <button type="submit">Predict</button>
+            </form>
+            <br>
+            <img id="uploadedImage" style="display:none; width: 100%; max-width: 400px; margin-top: 20px;"/>
+            <p id="analyzingMessage"></p>
+            <p id="result"></p>
+        </div>
         <script src="' . plugin_dir_url(__FILE__) . 'sap-script.js"></script>
     ';
 }
 add_shortcode('sap_form', 'sap_prediction_form');
+?>
